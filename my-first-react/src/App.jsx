5e5750704a3f3e7a8fae7ad5a54414ba26ac2d09
Mark Glasman <glasman.mark@gmail.com>
2023-11-13 16:39:12 -0500

    made an enormous breakthrough in working with React

--- my-first-react/src/App.jsx
+++ my-first-react/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setFeatPupId] = useState(null)
   const featuredPup = puppies.find((pup)=> pup.id === featPupId)
-  
+  console.log(featuredPup)
   return (
     <>
       <div>
@@ -15,8 +15,15 @@ function App() {
             })
            
         }
-        
-         { featPupId && <p>{ featPupId }</p> } 
+         {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
       
     
