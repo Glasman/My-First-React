9f8d9029ccb72919556574a81c9a2e0c34a37811
Mark Glasman <glasman.mark@gmail.com>
2023-11-13 16:22:14 -0500

    updated code

--- my-first-react/src/App.jsx
+++ my-first-react/src/App.jsx
@@ -1,12 +1,22 @@
-
-
+import {puppyList} from './data.js'
+import { useState } from 'react'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  
   return (
     <>
       <div>
+        {
+            puppies.map((puppy) => {
+              return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+              
+            })
+           
+        }
         
+         { featPupId && <p>{ featPupId }</p> } 
       </div>
       
     
