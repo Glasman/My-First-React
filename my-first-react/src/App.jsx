d66a772b10ac8af7ca31e8ea37561a21a491c26d
Mark Glasman <glasman.mark@gmail.com>
2023-11-13 15:05:18 -0500

    initial commit

---
+++ my-first-react/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>hi</p>
+      </div>
+      
+    
+    </>
+  )
+}
+
+export default App
